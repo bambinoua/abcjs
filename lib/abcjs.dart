57ebc618aedfe75eaca1ac94a15facf75cceeaf5
BambinoUA <45417992+bambinoua@users.noreply.github.com>
2026-07-24 15:58:26 +0300

    Export `abcjs`

--- lib/abcjs.dart
+++ lib/abcjs.dart
@@ -1,5 +1,6 @@
-export 'src/abcjs_interop_audio.dart';
+export 'abcjs_wrappers.dart';
+export 'src/abcjs_interop.dart';
 export 'src/abcjs_interop_anima.dart';
+export 'src/abcjs_interop_audio.dart';
 export 'src/abcjs_interop_callback.dart';
 export 'src/abcjs_interop_visual.dart';
-export 'abcjs_wrappers.dart';
